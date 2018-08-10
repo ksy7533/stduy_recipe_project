eb844747fed91591be2535f8d1f38e29c8774963
seyoon.kim <ksy7533@zuminternet.com>
2018-08-10 15:48:59 +0900

    axios함수를 이용하여 food2fork AIP 받아오기, proxy를 이용하여 cors 문제 해결

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-import str from './models/Search';
-import { add, multiply, ID } from './views/searchView';
+import axios from 'axios';
 
-console.log(`usijng imported function ${add(ID, 2)} and ${multiply(3, 5)}, ${str}`)
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'f70867036f71d2c442f17ab8ee98dc9e';
+
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getResults('pizza');
